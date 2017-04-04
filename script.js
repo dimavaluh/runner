0c540fc94e2909596627dfebf961df928e66fce3
Dmitry Valiukh <dimavaluh@gmail.com>
2017-04-04 16:51:39 +0300

    added movement with jQuery

--- script.js
+++ script.js
@@ -39,3 +39,52 @@ function mouseOut () {
 
 runner.onmouseover = mouseOver;
 runner.onmouseout = mouseOut;
+
+$(document).ready(function() {
+    animateDiv();
+
+});
+
+function makeNewPosition($field) {
+
+    // Get viewport dimensions (remove the dimension of the div)
+    $field = ($field || $(window));
+    var h = $field.height() - 50;
+    var w = $field.width() - 50;
+
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+
+    return [nh, nw];
+
+}
+
+function animateDiv() {
+    var $target = $('#runner');
+    var newq = makeNewPosition($target.parent());
+    var oldq = $target.offset();
+    var speed = calcSpeed([oldq.top, oldq.left], newq);
+
+    $('#runner').animate({
+        top: newq[0],
+        left: newq[1]
+    }, speed, function() {
+        animateDiv();
+    });
+
+};
+
+function calcSpeed(prev, next) {
+
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
+
+    var greatest = x > y ? x : y;
+
+    var speedModifier = 0.5;
+
+    var speed = Math.ceil(greatest / speedModifier);
+
+    return speed;
+
+}
